77de31937dc045dec908c82da8663ffc66c38562
Surin Lee <leesurin99@gmail.com>
2021-12-01 01:00:52 +0900

    상품 리스트 페이지 만들기

--- shop/models.py
+++ shop/models.py
@@ -2,15 +2,19 @@ from django.db import models
 
 # Create your models here.
 # product
-class Goods(models.Model):
+class Good(models.Model):
     # 상품명
     name = models.TextField()
     # 간단한 설명
-    about = models.CharField(max_length=500)
+    about = models.TextField()
     # 상품 가격
     price = models.IntegerField()
     # 상품 이미지
-    image = models.ImageField()
+    image = models.ImageField(upload_to='shop/images/', blank=False)
+
+    def __str__(self):
+        return f'{self.name}'
+
 # company which makes product
 
 
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Good
 
 # Create your views here.
+def index(request):
+    goods = Good.objects.all().order_by('pk')
+
+    return render(
+        request,
+        'shop/good_list.html',
+        {
+            'goods' : goods
+        }
+    )
