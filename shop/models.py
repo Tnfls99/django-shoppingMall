724be6a4bf855211549bcf7333fd2285e50917e9
Surin Lee <leesurin99@gmail.com>
2021-12-07 19:34:25 +0900

    회사와 사이즈 모델 형성 및 관계 설정

--- shop/models.py
+++ shop/models.py
@@ -3,6 +3,27 @@ from django.db import models
 # Create your models here.
 
 
+# company which makes product
+class Company(models.Model):
+    # 회사명
+    com_name = models.CharField(max_length=30)
+    #  회사 주소
+    address = models.TextField()
+    # 회사 연락처
+    contact = models.CharField(max_length=100)
+    # 회사 자사몰 링크
+    com_url = models.URLField()
+    # 회사 계좌번호
+    bank_account = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.com_name}'
+
+
+class Size(models.Model):
+    # 사이즈 종류를 위한 필드
+    version = models.CharField(max_length=20)
+
 # product
 class Good(models.Model):
     # 상품명
@@ -17,8 +38,10 @@ class Good(models.Model):
     image = models.ImageField(upload_to='shop/images/', blank=False)
     # 상품 상세페이지를 위한 자사몰 url
     from_url = models.URLField()
-
-
+    # 일대다 관계 (상품 - 회사)
+    company = models.ForeignKey(Company, null=True, on_delete=models.CASCADE)
+    # 상품 사이즈
+    size = models.ManyToManyField(Size)
 
     def __str__(self):
         return f'{self.name}'
@@ -26,6 +49,6 @@ class Good(models.Model):
     def get_absolute_url(self):
         return f'/shop/{self.pk}/'
 
-# company which makes product
+
 
 
